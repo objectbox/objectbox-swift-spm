4d9f5d70f748de2fc8a5c8116443aac951cd1050
Harald <noreply+harald@objectbox.io>
2024-10-29 14:47:34 +0100

    Adopt names for xcode usage

--- Package.swift
+++ Package.swift
@@ -2,13 +2,16 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
 let package = Package(
   name: "ObjectBox",
 
   products: [
     .plugin(name: "GeneratorCommand", targets: ["GeneratorCommand"]),
-    .library(name: "ObjectBox", targets: ["ObjectBox"]),
+    .library(
+      name: "ObjectBox.xcframework",  targets: ["ObjectBox.xcframework"]
+    ),
   ],
   targets: [
 
@@ -16,14 +19,14 @@ let package = Package(
     .binaryTarget(
       name: "ObjectBoxGenerator",
       url:
-        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/v1.3.x/ObjectBoxGenerator.artifactbundle.zip",
+        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/SPM-preview1/ObjectBoxGenerator.artifactbundle.zip",
       checksum: "62d3e8d9e7141ef75462c8f8f08e6334def0b80f946053550ff7c6789c3187f9"
     ),
     .binaryTarget(
-      name: "ObjectBox",
+      name: "ObjectBox.xcframework",
       url:
-        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/v1.3.x/ObjectBox.xcframework.zip",
-      checksum: "b680d60d598f818d5f077eebbe0584bd38b723c982133bc186ffe18bf6364eb2"
+        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/SPM-preview1/ObjectBox.xcframework.zip",
+      checksum: "3222f5d3eec6f91a1d47f3a7acfec47c99bd7b2eeec27948fddd58bb8a9dd18e"
     ),
 
     /// MARK: - Plugin implementations
@@ -46,3 +49,21 @@ let package = Package(
 
   ]
 )
+
+// TODO, we need to be able to switch between CI and release in the URL, but keep the checksum the same
+// additionally, having an option for local development could also be handy
+// see if this, or any other way could be used ...
+// enum BinarySource {
+//     case local, staging, release
+
+//     init() {
+//         if getenv("OBX_SPM_LOCAL_BINARIES") != nil {
+//             self = .local
+//         } else if getenv("OBX_SPM_STAGING_BINARIES") != nil  {
+//             self = .staging
+//         } else {
+//             self = .release
+//         }
+//     }
+// }
+
