ddeaefae773a50b0b777fa0d34b5265e3e5204ff
Harald <noreply+harald@objectbox.io>
2024-11-01 14:34:56 +0100

    Add some temporary diagnostig output, to trace fetch behaviour

--- Plugins/GeneratorCommand/ObjectBoxCommand.swift
+++ Plugins/GeneratorCommand/ObjectBoxCommand.swift
@@ -78,13 +78,21 @@ struct GeneratorCommand: CommandPlugin {
       ? context.package.targets
       : try context.package.targets(named: targetNames)
 
+
     // Since the generator generates code even for targets that have no ObjectBox annotations at all,
     // restrict the target to not generate code for targets like tests ...
     if targets.count > 1 {
-      let targetNames = targets.map { $0.name }.joined(separator: ", ")
-      Diagnostics.error("Multiple targets found\nPlease select specify one target by using the `--target name` option\nAvailable target names: \(targetNames)")
+      let availableTargetNames = targets.map { $0.name }.joined(separator: ", ")
+      Diagnostics.error("Multiple targets found\nPlease select specify one target by using the `--target name` option\nAvailable target names: \(availableTargetNames)")
       return
+    } else if targets.isEmpty {
+      Diagnostics.error("No target found")
+      return
+    } else {
+      print("Have one target \(targets[0].name)")
     }
+
+    print("Remove updated version 1") // some debug to see if latest git version is fetched, or if some cache is used
 
     for target in targets {
       guard let target = target.sourceModule else { continue }
