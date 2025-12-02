516b73a135b02f98f7b02046c50b74708373f312
Uwe <noreply+uwe@objectbox.io>
2025-12-02 15:09:11 +0100

    Release 5.1.0-rc.1

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   name: "ObjectBox",
   platforms: [
     // This should match the requirements of ObjectBox.xcframework (so the ObjectBox Swift API and native libraries)
-    .macOS(.v10_15), .iOS(.v12),
+    .macOS(.v11), .iOS(.v15),
   ],
   products: [
     .plugin(name: "ObjectBoxPlugin", targets: ["ObjectBoxGeneratorCommand"]),
@@ -22,20 +22,20 @@ let package = Package(
     .binaryTarget(
       name: "ObjectBoxGenerator",
       url:
-        "https://github.com/objectbox/objectbox-swift-spm/releases/download/5.0.0/ObjectBoxGenerator.artifactbundle.zip",
-      checksum: "39aa952e7250b58b956fa37adad0fe500255db5960df2a207071ea5556d62d7d"
+        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/v5.1.0-rc.1/ObjectBoxGenerator.artifactbundle.zip",
+      checksum: "5f10e5cef769461290e1c30d51c647bc36955b5d7af0f4f09ffb9a90de3cdb54"
     ),
     .binaryTarget(
       name: "ObjectBox.xcframework",
       url:
-        "https://github.com/objectbox/objectbox-swift-spm/releases/download/5.0.0/ObjectBox.xcframework.zip",
-      checksum: "3aeda304fa972e95f08d386784ab8ae220e6850f650ec07f46f8e926f5e4655a"
+        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/v5.1.0-rc.1/ObjectBox.xcframework.zip",
+      checksum: "92c7567307a185d7bfc20c2af136cbe49da8309865af1cdf4f48ce2443c399c1"
     ),
     .binaryTarget(
       name: "ObjectBox-Sync.xcframework",
       url:
-        "https://github.com/objectbox/objectbox-swift-spm/releases/download/5.0.0/ObjectBox-Sync.xcframework.zip",
-      checksum: "34cf4555c14d177a7435e9167203196237dd2af614b0a369a9731fb533555fa9"
+        "https://github.com/objectbox/objectbox-swift-spec-staging/releases/download/v5.1.0-rc.1/ObjectBox-Sync.xcframework.zip",
+      checksum: "107736cf61cd7d8e1f72bbdb9208df617568868545c6a2116d7ec0f73a096c61"
     ),
 
     /// MARK: - Plugin implementations
